dda9220e7259903ad74d62992e248b4bbf77a1bd
AnastasiaBatova <batosha9913@icloud.com>
2024-11-21 13:58:28 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,14 @@
+# "1st program"
+print(9**0.5*5)
+# "2nd program
+print (9.99>9/98 and 1000!=1000.1)
+# "3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*92+2))
+# " 4th program"
+print('123.456')
+print(123.456*10)
+print(1234.56*10)
+print(12345.6*10)
+print(123456.0%10)
